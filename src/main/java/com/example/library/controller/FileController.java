553cc1f40497327bcb3c24934e03739593d4a9c8
Sauatdev <zhasbolatov04@gmail.com>
2025-07-07 14:47:33 +0500

    add: created new entity for work with files and service controller

---
+++ src/main/java/com/example/library/controller/FileController.java
@@ -0,0 +1,26 @@
+package com.example.library.controller;
+
+import com.example.library.entity.FileE;
+import com.example.library.service.FileEService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/files")
+@Slf4j
+@CrossOrigin("*")
+public class FileController {
+    private final FileEService fileEService;
+
+
+    @PostMapping
+    public FileE uploadFileF(@RequestParam("file") MultipartFile file) throws IOException {
+        return fileEService.uploadFile(file);
+    }
+}
---
+++ src/main/java/com/example/library/repository/FileERepository.java
@@ -0,0 +1,7 @@
+package com.example.library.repository;
+
+import com.example.library.entity.FileE;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileERepository extends JpaRepository <FileE,Long> {
+}
